b6138fab7caf324ef5d57339c19ceed55300c01c
piotr <mikar3@o2.pl>
2022-02-07 16:59:54 +0100

    .

---
+++ VulnManager/Controllers/ServerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VulnManager.Models;
+using VulnManager.Data;
+
+namespace VulnManager.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ServerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var server = _context.Servers.Where(s => s.Id == id).FirstOrDefault();
+            if (server == null)
+            {
+                return NotFound();
+            }
+            return View(server);
+        }
+
+
+
+
+
+    }
+}
