50f9bda274b00403f3828f38e9ceb3f97f69d243
piotr <mikar3@o2.pl>
2022-02-10 21:35:35 +0100

    .

---
+++ VulnManager/Models/CveInfo.cs
@@ -0,0 +1,160 @@
+﻿namespace VulnManager.Models
+{
+    //public class CveInfo
+    //{
+    //    public Impact impact { get; set; }
+    //    public CVEDataMeta CVE_data_meta { get; set; }
+    //}
+
+    //public class CVEDataMeta
+    //{
+    //    public string ID { get; set; }
+    //}
+
+    //public class Impact
+    //{
+    //    public BaseMetricV3 baseMetricV3 { get; set; }
+    //}
+
+    //public class BaseMetricV3
+    //{
+    //    public CvssV3 cvssV3 { get; set; }
+    //}
+
+    //public class CvssV3
+    //{
+    //    public double baseScore { get; set; }
+    //}
+
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class CVEDataMeta
+    {
+        public string ID { get; set; }
+        public string ASSIGNER { get; set; }
+    }
+
+    public class Description
+    {
+        public string lang { get; set; }
+        public string value { get; set; }
+        public List<DescriptionData> description_data { get; set; }
+    }
+
+    public class ProblemtypeData
+    {
+        public List<Description> description { get; set; }
+    }
+
+    public class Problemtype
+    {
+        public List<ProblemtypeData> problemtype_data { get; set; }
+    }
+
+    public class ReferenceData
+    {
+        public string url { get; set; }
+        public string name { get; set; }
+        public string refsource { get; set; }
+        public List<string> tags { get; set; }
+    }
+
+    public class References
+    {
+        public List<ReferenceData> reference_data { get; set; }
+    }
+
+    public class DescriptionData
+    {
+        public string lang { get; set; }
+        public string value { get; set; }
+    }
+
+    public class CpeMatch
+    {
+        public bool vulnerable { get; set; }
+        public string cpe23Uri { get; set; }
+        public string versionEndExcluding { get; set; }
+        public List<object> cpe_name { get; set; }
+    }
+
+    public class Node
+    {
+        public string @operator { get; set; }
+        public List<object> children { get; set; }
+        public List<CpeMatch> cpe_match { get; set; }
+    }
+
+    public class Configurations
+    {
+        public string CVE_data_version { get; set; }
+        public List<Node> nodes { get; set; }
+    }
+
+    public class CvssV3
+    {
+        public string version { get; set; }
+        public string vectorString { get; set; }
+        public string attackVector { get; set; }
+        public string attackComplexity { get; set; }
+        public string privilegesRequired { get; set; }
+        public string userInteraction { get; set; }
+        public string scope { get; set; }
+        public string confidentialityImpact { get; set; }
+        public string integrityImpact { get; set; }
+        public string availabilityImpact { get; set; }
+        public double baseScore { get; set; }
+        public string baseSeverity { get; set; }
+    }
+
+    public class BaseMetricV3
+    {
+        public CvssV3 cvssV3 { get; set; }
+        public double exploitabilityScore { get; set; }
+        public double impactScore { get; set; }
+    }
+
+    public class CvssV2
+    {
+        public string version { get; set; }
+        public string vectorString { get; set; }
+        public string accessVector { get; set; }
+        public string accessComplexity { get; set; }
+        public string authentication { get; set; }
+        public string confidentialityImpact { get; set; }
+        public string integrityImpact { get; set; }
+        public string availabilityImpact { get; set; }
+        public double baseScore { get; set; }
+    }
+
+    public class BaseMetricV2
+    {
+        public CvssV2 cvssV2 { get; set; }
+        public string severity { get; set; }
+        public double exploitabilityScore { get; set; }
+        public double impactScore { get; set; }
+        public bool acInsufInfo { get; set; }
+        public bool obtainAllPrivilege { get; set; }
+        public bool obtainUserPrivilege { get; set; }
+        public bool obtainOtherPrivilege { get; set; }
+        public bool userInteractionRequired { get; set; }
+    }
+
+    public class Impact
+    {
+        public BaseMetricV3 baseMetricV3 { get; set; }
+        public BaseMetricV2 baseMetricV2 { get; set; }
+    }
+
+    public class CveInfo
+    {
+        public Cve cve { get; set; }
+        public Configurations configurations { get; set; }
+        public Impact impact { get; set; }
+        public string publishedDate { get; set; }
+        public string lastModifiedDate { get; set; }
+    }
+
+
+
+}
---
+++ VulnManager/Services/CveDataGetter.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.Text.Json;
+using VulnManager.Data;
+using VulnManager.Models;
+
+namespace VulnManager.Services
+{
+    public class CveDataGetter
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly HttpClient _httpClient;
+        private ApplicationDbContext context;
+
+        public CveDataGetter(ApplicationDbContext context, HttpClient httpClient)
+        {
+            _context = context;
+            _httpClient = httpClient;
+        }
+
+        public async Task ScanCvesAsync(ApplicationDbContext context)
+        {
+            var cves = await _context.Cves.ToListAsync();
+            if (cves is null)
+                return;
+            foreach (var cve in cves)
+            {
+                var url = new Uri("https://olbat.github.io/nvdcve/" + cve.Name + ".json");
+                var cveInfo = await ScanCveAsync(cve.Name, url);
+                await UpdateCvssAsync(cveInfo, cve, context);
+            }
+        }
+
+        public async Task UpdateCvssAsync(CveInfo cveInfo, Cve cve, ApplicationDbContext _context)
+        {
+            if (cveInfo == null)
+            {
+                return;
+            }
+            cve.CVSS = cveInfo.impact.baseMetricV2.cvssV2.baseScore;
+            _context.Update(cve);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<CveInfo> ScanCveAsync(string ip, Uri uri)
+        {
+            var cveResponse = await _httpClient.GetAsync(uri);
+            if (cveResponse.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Error: code: " + cveResponse.StatusCode.ToString());
+            }
+            var cveResponseString = await cveResponse.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<CveInfo>(cveResponseString);
+        }
+    }
+}
