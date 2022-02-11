db2889678f64ab89c642a219b9493ac0b77325d7
piotr <mikar3@o2.pl>
2022-02-11 14:03:57 +0100

    .

---
+++ VulnManager/Controllers/CVEController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VulnManager.Models;
+using VulnManager.Data;
+using VulnManager.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace VulnManager.Controllers
+{
+    public class CVEController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CVEController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var cves = _context.Cves.ToList();
+            return View(cves);
+        }
+    }
+}
---
+++ VulnManager/Controllers/PortController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VulnManager.Models;
+using VulnManager.Data;
+using VulnManager.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace VulnManager.Controllers
+{
+    public class PortController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PortController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var ports = _context.Ports.Include(s => s.Server).ToList();
+            return View(ports);
+        }
+    }
+}
